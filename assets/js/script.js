595574446b69d86c09bd8ca00bf64af3ea15ae98
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-07-20 06:44:43 +0000

    Select elements that will change theme colour

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Changing the theme
+let mode;
+var moon = document.getElementById("moon");
+var moonLight = document.getElementById("moonLight");
+var night = document.getElementById("night");
+var navbar = document.querySelector(".navbar");
+var heading3 = navbar.querySelector("h3");
+var title = document.querySelector(".card-body .card-title");
+var body = document.querySelector("body");
+var count = document.querySelector(".count");
+var countNo = document.querySelector(".count span");
+var card = document.getElementsByClassName("habbit-card");
+var modal = document.querySelector(".modal-content");
+var modalInput = document.querySelector(".new-habit-input");
+var footer = document.querySelector(".footer-section");
